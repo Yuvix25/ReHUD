fcad8da84e80fdaeb353bcd13cd5bed69d93759b
Rich Evans <rcb.evans@outlook.com>
2024-02-19 18:13:24 -0800

    Check sharedMemory rather than raceroom observer

--- Startup.cs
+++ Startup.cs
@@ -445,7 +445,7 @@ public class Startup
                     enteredEditMode = true;
                     IsInEditMode = true;
 
-                    if (!raceroomObserver.IsRunning)
+                    if (!sharedMemory.IsRunning)
                     {
                         var extraData = new ExtraData
                         {
@@ -466,7 +466,7 @@ public class Startup
                 isShown = null;
                 IsInEditMode = false;
 
-                if (!raceroomObserver.IsRunning)
+                if (!sharedMemory.IsRunning)
                 {
                     Electron.IpcMain.Send(MainWindow, "hide");
                 }
